0ba9ffb44e10a690de0ae1755e2074ce552ca58f
nicatmazanli <47052509+nicatmazanli@users.noreply.github.com>
2020-11-02 10:41:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 
-sudo chmod +x banner.sh
+chmod +x banner.sh
 ./banner.sh
 
 echo "Updating system"
 echo "sudo apt update %% sudo apt upgrade"
 echo "\n"
 echo "Installling curl"
-sudo apt-get install curl
+apt-get install curl
 
-sudo chmod +x Telegram_Bot\ _Message_Sender.py 
+chmod +x Telegram_Bot\ _Message_Sender.py 
 ./Telegram_Bot\ _Message_Sender.py   
